203c09ac5d9220c644f9fd6a7878a32c3b226fc3
Alexander Hutorezki <huto@wa-gruppe.com>
2020-09-26 00:01:02 +0200

    chore: Introduce config component

---
+++ README.md
@@ -0,0 +1,10 @@
+## Waglpz config component
+
+The Library enables you to work with web application as MVC or RESTful API or both.
+
+### Requirements
+
+PHP 8.2 or higher (see composer json)
+
+### Installation
+ coming soon ...
---
+++ config/main.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+\Locale::setDefault('de_DE.utf8');
+
+return [
+    'anonymizeLog'         => [
+        '_SERVER' => [
+            'DB_PASSWD' => '*****',
+            'DB_USER'   => '*****',
+        ],
+        '_POST'   => [/* set here necessary keys wich should be anonymized in log*/],
+    ],
+    'logErrorsDir'         => '/tmp',
+//    'router'               => include 'router.php',
+//    'db'                   => include 'db.php',
+    'logger'               => include 'logger.php',
+//    'sso'                  => include 'sso.php',
+//    'redirect_after_login' => '/',
+
+//    'view'                 => [
+//        'templates'  => \dirname(__DIR__) . '/templates/',
+//        'attributes' => ['webseitenTitle' => 'Testseite'],
+//        'layout'     => 'layout.phtml',
+//    ],
+    //uncomment to enable exception handler
+//    'exception_handler'    => ExceptionHandler::class,
+
+    //install waglpz/webapp-security component and uncomment to enable firewall
+//    'firewall'             => include 'firewall.php',
+
+//    'apiVersion'           => '0.1.0',
+//    'swagger_scheme_file'  => __DIR__ . '/../swagger.json',
+];
